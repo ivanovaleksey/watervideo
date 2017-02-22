23d7dd749b8f3a2c307286b32e3b38c0fa72f1d7
Aleksey Ivanov <ialexxei@gmail.com>
2017-02-23 00:54:22 +0300

    Implement SSE

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'videos#index'
 
-  resources :videos, except: :destroy
+  resources :videos, except: :destroy do
+    member do
+      get :thumbnail_state
+      get :file_state
+    end
+  end
 end
